41eb0da2dc6572da36b54c944e40342f5db25731
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-04-04 19:15:04 +0800

    Fix bug for forgetting to destructure project object

--- src/Project.js
+++ src/Project.js
@@ -1,3 +1,3 @@
-export default function Project(name, color = "grey", favorited = false) {
+export default function Project({ name, color = "grey", favorited = false }) {
   return { name, color, favorited };
 }
