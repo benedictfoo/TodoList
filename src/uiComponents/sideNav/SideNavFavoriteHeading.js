441c87a31fc84aa9f01adabdbb4de3a5f726c4ed
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-04-07 18:51:51 +0800

    Remove unneeded repetitive modules

--- src/uiComponents/sideNav/SideNavFavoriteHeading.js
+++
@@ -1,9 +0,0 @@
-import createElement from "../createElement";
-export default function () {
-  const heading = createElement(
-    "span",
-    { class: "nav-favorite-heading" },
-    "Favorites"
-  );
-  return heading;
-}
--- src/uiComponents/sideNav/SideNavProjectHeading.js
+++
@@ -1,9 +0,0 @@
-import createElement from "../createElement";
-export default function () {
-  const heading = createElement(
-    "span",
-    { class: "nav-project-heading" },
-    "Projects"
-  );
-  return heading;
-}
