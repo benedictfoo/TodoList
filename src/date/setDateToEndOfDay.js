5f1e8e2a8362ca1a36da3861def082f1be8f1734
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-04-07 11:42:15 +0800

    Add date-fns and an uncompleted date function

---
+++ src/date/setDateToEndOfDay.js
@@ -0,0 +1,5 @@
+import { set } from "date-fns";
+
+export default function (dateObject) {
+  return set(dateObject);
+}
--- src/taskTreeFunctions(toremove)/findSubTaskDepth.js
+++
@@ -1,5 +0,0 @@
-export default function findSubTaskDepth(Store, taskId, depth = 0) {
-  const task = Store.find((t) => t.id === taskId);
-  if (!task?.parentId) return depth;
-  return findSubTaskDepth(Store, task.parentId, depth + 1);
-}
