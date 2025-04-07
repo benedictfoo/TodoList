4691e6dd980597224edba50212a0ad2251b750e3
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-04-07 11:59:18 +0800

    Able to link a test function to my index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import "./style.css";
+import {
+  updateProjectByIdFromStore,
+  getProjectsFromStore,
+} from "./store/ProjectStore";
 
 function component() {
   const element = document.createElement("div");
-
+  updateProjectByIdFromStore("f24be39c-b029-4fac-8a8c-07f9d57b9a79", {
+    name: "PROJECT ZERO",
+    color: "HotPink",
+    favorited: true,
+  });
+  console.log(getProjectsFromStore());
   return element;
 }
 
