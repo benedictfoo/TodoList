a3b00480f2a7710f1bac9510d64f9ba6ce18ece9
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-04-08 15:09:12 +0800

    Make a function to render outside sidenav the tasks

--- src/events/sideNavEvents.js
+++ src/events/sideNavEvents.js
@@ -1,5 +1,4 @@
-import mainContent from "../uiComponents/mainContent/mainContent";
-import removeMainContent from "../uiComponents/mainContent/removeMainContent";
+import renderMainContent from "../renderMainContent/renderMainContent";
 
 export default function () {
   document.querySelector(".nav").addEventListener("click", (e) => {
@@ -11,22 +10,6 @@ export default function () {
       navItemRow.classList.add("active");
     }
 
-    if (navItemRow.closest(".nav-item-inbox-row")) {
-      removeMainContent();
-      document.body.appendChild(mainContent({ completed: false }, "Inbox"));
-    } else if (navItemRow.closest(".nav-item-today-row")) {
-      removeMainContent();
-      document.body.appendChild(mainContent({ date: 1111 }, "Today"));
-    } else if (navItemRow.closest(".nav-item-upcoming-row")) {
-      removeMainContent();
-      document.body.appendChild(mainContent({ date: 111 }, "Upcoming"));
-    } else if (navItemRow.closest(".nav-item-completed-row")) {
-      removeMainContent();
-      document.body.append(mainContent({ completed: true }, "Completed"));
-    } else if (navItemRow.closest(".nav-project-row")) {
-      removeMainContent();
-      const projectId = navItemRow.closest(".nav-project-row").dataset.id;
-      document.body.appendChild(mainContent({ projectId }, "Upcoming"));
-    }
+    renderMainContent(navItemRow);
   });
 }
---
+++ src/renderMainContent/renderMainContent.js
@@ -0,0 +1,22 @@
+import mainContent from "../uiComponents/mainContent/mainContent";
+import removeMainContent from "../uiComponents/mainContent/removeMainContent";
+
+export default function (navItemRow) {
+  if (navItemRow.closest(".nav-item-inbox-row")) {
+    removeMainContent();
+    document.body.appendChild(mainContent({ completed: false }, "Inbox"));
+  } else if (navItemRow.closest(".nav-item-today-row")) {
+    removeMainContent();
+    document.body.appendChild(mainContent({ date: 1111 }, "Today"));
+  } else if (navItemRow.closest(".nav-item-upcoming-row")) {
+    removeMainContent();
+    document.body.appendChild(mainContent({ date: 111 }, "Upcoming"));
+  } else if (navItemRow.closest(".nav-item-completed-row")) {
+    removeMainContent();
+    document.body.append(mainContent({ completed: true }, "Completed"));
+  } else if (navItemRow.closest(".nav-project-row")) {
+    removeMainContent();
+    const projectId = navItemRow.closest(".nav-project-row").dataset.id;
+    document.body.appendChild(mainContent({ projectId }, "Upcoming"));
+  }
+}
